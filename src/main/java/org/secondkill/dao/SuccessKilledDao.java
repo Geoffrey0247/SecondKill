5e596d2856988976d9c31016eda54b67ac39741b
Geoffrey0247 <18912227221@163.com>
2017-03-18 15:47:57 +0800

    add dao

---
+++ src/main/java/org/secondkill/dao/SuccessKilledDao.java
@@ -0,0 +1,24 @@
+package org.secondkill.dao;
+
+import org.apache.ibatis.annotations.Param;
+import org.secondkill.entity.SuccessKilled;
+
+/**
+ * Created by GBC on 2017/3/18.
+ */
+public interface SuccessKilledDao {
+    /**
+     * 插入购买明细，可过滤重复
+     * @param seckillId
+     * @param userPhone
+     * @return 插入行数
+     */
+    int insertSuccessKilled(@Param("seckillId") long seckillId, @Param("userPhone") long userPhone);
+
+    /**
+     * 根据id查询Successkilled并携带秒杀产品对象实体
+     * @param seckillId
+     * @return
+     */
+    SuccessKilled queryByIdWithSeckill(@Param("seckillId") long seckillId, @Param("userPhone") long userPhone);
+}
