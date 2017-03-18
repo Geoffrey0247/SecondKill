40f8bbdfdb1763b1272c87f56cca2f42a293bd29
Geoffrey0247 <18912227221@163.com>
2017-03-18 15:48:43 +0800

    add dto

---
+++ src/main/java/org/secondkill/dto/SeckillExecution.java
@@ -0,0 +1,74 @@
+package org.secondkill.dto;
+
+import org.secondkill.entity.SuccessKilled;
+import org.secondkill.enums.SeckillStatEnum;
+
+/** 封装秒杀执行结果
+ * Created by GBC on 2017/3/18.
+ */
+public class SeckillExecution {
+    private long seckillId;
+
+    //秒杀结果状态
+    private int state;
+
+    //状态标识
+    private String stateInfo ;
+
+    //秒杀成功的对象
+    private SuccessKilled successKilled;
+
+    public SeckillExecution(long seckillId, SeckillStatEnum statEnum, SuccessKilled successKilled) {
+        this.seckillId = seckillId;
+        this.state = statEnum.getState();
+        this.stateInfo = statEnum.getStateInfo();
+        this.successKilled = successKilled;
+    }
+
+    public SeckillExecution(long seckillId, SeckillStatEnum statEnum) {
+        this.seckillId = seckillId;
+        this.state = statEnum.getState();
+        this.stateInfo = statEnum.getStateInfo();
+    }
+
+    @Override
+    public String toString() {
+        return "SeckillExecution{" +
+                "seckillId=" + seckillId +
+                ", state=" + state +
+                ", stateInfo='" + stateInfo + '\'' +
+                ", successKilled=" + successKilled +
+                '}';
+    }
+    public long getSeckillId() {
+        return seckillId;
+    }
+
+    public void setSeckillId(long seckillId) {
+        this.seckillId = seckillId;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    public String getStateInfo() {
+        return stateInfo;
+    }
+
+    public void setStateInfo(String stateInfo) {
+        this.stateInfo = stateInfo;
+    }
+
+    public SuccessKilled getSuccessKilled() {
+        return successKilled;
+    }
+
+    public void setSuccessKilled(SuccessKilled successKilled) {
+        this.successKilled = successKilled;
+    }
+}
